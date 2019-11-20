5da7ca240416844f0ef33dd638451790cd5b2c30
Marius Drąsutis <marius.dras@gmail.com>
2019-11-20 18:28:21 +0200

    Guest & admin container views

---
+++ src/views/Guest.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <guest-nav></guest-nav>
+        <router-view />
+    </div>
+</template>
+
+<script>
+import GuestNav from './nav/guest/Navigation'
+
+export default {
+    components: { GuestNav }
+}
+</script>
