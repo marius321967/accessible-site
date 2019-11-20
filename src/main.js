8085ebed820f86357e8ae73aaa17fc4ef113e11d
Marius Drąsutis <marius.dras@gmail.com>
2019-11-20 17:03:56 +0200

    Added fontawesome to main.js, loading one icon

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faWheelchair } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faWheelchair)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
+    router,
+    store,
+    render: function (h) { return h(App) }
 }).$mount('#app')
