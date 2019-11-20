1c927e8cd0b30623d1c8c96c9121e6ec9182ddcc
Marius Drąsutis <marius.dras@gmail.com>
2019-11-20 18:28:10 +0200

    Styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,28 @@
 
 <style lang="scss">
 @import 'bootstrap';
+
+body {
+    font-weight: 500;
+}
+
+.standard-page {
+    @extend .container;
+    margin-top: 1rem;
+}
+
+label.label {
+    text-transform: uppercase;
+}
+
+.form-group {
+    margin-bottom: 2rem;
+}
+
+.primary-links .nav-item .nav-link {
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
 </style>
