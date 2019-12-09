fe9835aedb067f9bb7a2dfb0ecaec3981c758a65
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 22:00:19 +0200

    Updated App.vue for accessibility (unfinished)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
     <div>
         <router-view />
+        
+        <b-popover :target="popoverElement" v-if="popoverElement" :show="true" triggers="manual" placement="bottomleft">
+            <template v-slot:title>Popover Title</template>
+            <button>Speak</button>
+        </b-popover>
     </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    beforeCreate() {
+        this.$store.dispatch('authLoadToken');
+    },
+    mounted() {
+        const store = this.$store;
+
+        document.addEventListener('selectionchange', e => {
+            const isDeselected = document.getSelection().toString() == '';
+            const element = document.getSelection().focusNode.parentElement;
+            // console.log({ top: element.offsetTop, left: element.offsetLeft });
+
+            if (isDeselected)   store.commit('POPOVER_SET_ELEMENT', null);
+            else                store.commit('POPOVER_SET_ELEMENT', element);
+        });
+    },
+
+    computed: { ...mapGetters(['popoverElement']) }
+}
+</script>
 
 <style lang="scss">
 @import 'bootstrap';
