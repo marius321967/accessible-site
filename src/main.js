8fd01638cd6d0e567182c3fe4791b50e92efc943
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 21:56:28 +0200

    Register bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faWheelchair } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import BootstrapVue from 'bootstrap-vue'
 
 library.add(faWheelchair)
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
     router,
