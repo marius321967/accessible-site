51e012414135f47e3dcfb24984cd0ece9916b4b7
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 21:56:51 +0200

    Added admin nav

---
+++ src/views/nav/admin/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div class="nav flex-column nav-pills">
+            <router-link to="/admin" class="nav-link active">Home</router-link>
+            <router-link to="/admin/products" class="nav-link">Products</router-link>
+            <router-link to="/admin/users" class="nav-link">Users</router-link>
+        </div>
+    </div>
+</template>
