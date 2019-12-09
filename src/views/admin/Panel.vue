1205b3b3a9267a3517782e393327c7a16f9ddf4f
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 21:57:14 +0200

    Admin panel first page

---
+++ src/views/admin/Panel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h5>Statistics</h5>
+        
+        <ul class="list-group">
+            <li class="list-group-item">Users: {{ users.length }}</li>
+            <li class="list-group-item">Products: 0</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import get_all_users from '@/services/api/users/get_all'
+
+export default {
+    data: () => ({ users: [] }),
+
+    created() {
+        get_all_users().then(users => this.users = users);
+    }
+}
+</script>
+
+<style>
+
+</style>
