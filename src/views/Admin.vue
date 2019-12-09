d675a5cfdb49d47ce70c72f15674cf0d08927000
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 21:57:05 +0200

    Added admin base component

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-        admin
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2 col-navigation">
+                <navigation></navigation>
+            </div>
+            <div class="col-lg-10 col-page-content"><router-view></router-view></div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import Navigation from '@/views/nav/admin/Navigation'
 
+export default {
+    components: { Navigation }
 }
 </script>
+
+<style>
+.col-page-content, .col-navigation {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+}
+</style>
