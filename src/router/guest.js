437c65d03c3dd64cc4faeeb464b9468e68fe13a6
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 21:59:09 +0200

    Add register route

--- src/router/guest.js
+++ src/router/guest.js
@@ -1,6 +1,7 @@
 import Guest from '../views/Guest.vue'
 import Home from '@/views/guest/Home'
 import Login from '@/views/guest/Login'
+import Register from '@/views/guest/Register'
 
 export default { 
     path: '/', 
@@ -8,5 +9,6 @@ export default {
     children: [
         { path: '/', component: Home },
         { path: 'login', component: Login },
+        { path: 'register', component: Register },
     ]
 }
