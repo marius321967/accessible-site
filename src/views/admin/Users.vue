174d0e2e3506f605ab679510d6db6a6588e252e6
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 21:58:08 +0200

    User administration components

---
+++ src/views/admin/Users.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-md-4">
+                <ul class="list-group">
+                    <user-item v-for="user in users" v-bind:key="user._id" :user="user"></user-item>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import get_all_users from '@/services/api/users/get_all'
+import UserItem from './users/UserItem'
+
+export default {
+    components: { UserItem },
+    data: () => ({ users: [] }),
+
+    created() {
+        get_all_users().then(users => this.users = users);
+    }
+}
+</script>
---
+++ src/views/admin/users/UserItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li class="list-group-item d-flex">
+        <div class="mr-auto d-inline-block"><router-link :to="user._id">{{ user.email }}</router-link></div>
+        
+        <div class="btn-group d-inline-block">
+            <button type="button" class="btn btn-sm btn-secondary">Delete</button>
+            <button type="button" class="btn btn-sm btn-secondary">Disable</button>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: { 
+        user: { required: true, type: Object }
+    }
+}
+</script>
