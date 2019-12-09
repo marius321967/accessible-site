c1f0601c4dba8925588986158a079dd3f1db9404
Marius Drąsutis <marius.dras@gmail.com>
2019-12-09 22:00:03 +0200

    Registered all admin routes

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,9 +1,18 @@
 import Admin from '@/views/Admin'
+import Panel from '@/views/admin/Panel'
+import Users from '@/views/admin/Users'
+import Products from '@/views/admin/Products'
+import ProductsNew from '@/views/admin/products/New'
+import ProductsView from '@/views/admin/products/View'
 
 export default {
     path: '/admin',
     component: Admin,
     children: [
-
+        { path: '', component: Panel },
+        { path: 'users', component: Users },
+        { path: 'products', component: Products },
+        { path: 'products/new', component: ProductsNew },
+        { path: 'products/:id', component: ProductsView, props: true },
     ]
 }
