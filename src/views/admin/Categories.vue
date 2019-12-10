3c18dc96ad649c50324cd5765b936cf8c0b619d0
Marius Drąsutis <marius.dras@gmail.com>
2019-12-10 20:48:10 +0200

    Categories page

---
+++ src/views/admin/Categories.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>
+            <router-link to="/admin/categories/new" class="btn btn-sm btn-success">Create category</router-link>
+        </p>
+
+        <div class="row">
+            <div class="col-md-4">
+                <ul class="list-group">
+                    <category-item v-for="category in categories" v-bind:key="category._id" :category="category"></category-item>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import get_all_categories from '@/services/api/categories/get_all'
+import CategoryItem from './categories/CategoryItem'
+
+export default {
+    components: { CategoryItem },
+    data: () => ({ categories: [] }),
+    
+    created() {
+        get_all_categories().then(categories => this.categories = categories);
+    }
+}
+</script>
---
+++ src/views/admin/categories/CategoryItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li class="list-group-item d-flex">
+        <div class="mr-auto d-inline-block">
+            <router-link :to="'/admin/categories/' + category._id">{{ category.name }}</router-link>
+        </div>
+        
+        <div class="btn-group d-inline-block">
+            <button type="button" class="btn btn-sm btn-secondary">Delete</button>
+            <button type="button" class="btn btn-sm btn-secondary">Hide</button>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props: { 
+        category: { required: true, type: Object }
+    }
+}
+</script>
