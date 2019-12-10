955cd75b03aed5116aec82dbfd358907a6b7c4c5
Marius Drąsutis <marius.dras@gmail.com>
2019-12-10 22:01:06 +0200

    Product page

--- src/views/guest/explore/Product.vue
+++ src/views/guest/explore/Product.vue
@@ -1,35 +1,84 @@
 <template>
-    <div class="standard-page">
-        <div class="row">
+    <div class="standard-page product-page">
+        <div class="row" v-if="product">
             <div class="col-md-6">
-                image
+                <div class="product-images-container">
+                    <aic class="accessible-images" :product-id="id"></aic>
+                    <nic class="nonaccessible-images" :product-id="id"></nic>
+                </div>
 
-                title
+                <h4 class="product-name">{{ product.name }}</h4>
 
-                description
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a pharetra quam, et condimentum risus. 
+                    Phasellus finibus libero id massa tincidunt bibendum. Suspendisse facilisis diam purus, in viverra 
+                    diam faucibus ac. Proin sit amet bibendum lorem. Cras a augue eu ligula tincidunt tempus. 
+                </p>
             </div>
 
             <div class="col-md-6">
-                price
+                <div class="price-container">
+                    <div class="price-label">Price</div>
+                    <div class="price">{{ product.price }}€</div>
+                    
+                </div>
 
-                addtocart
+                <div class="cart-options-container">
+                    <button class="btn btn-primary">Add to cart</button>
+                </div>
 
-                fake reviews with stars
+                <hr>
+
+                <div>
+                    <h6>Reviews</h6>
+                    <div class="stars">
+                        <font-awesome-icon icon="star" />
+                        <font-awesome-icon icon="star" />
+                        <font-awesome-icon icon="star" />
+                        <font-awesome-icon icon="star" />
+                        <font-awesome-icon :icon="['far', 'star']" />
+                    </div>
+                    <p> Average 4 <small> / </small> 5 </p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import aic from './product/AccessibleImagesCarousel'
+import nic from './product/NonaccessibleImagesCarousel'
+import get_product_by_id from '@/services/api/products/get_by_id'
+
 export default {
-    props: { productId: { required: true, type: String } },
+    props: { id: { required: true, type: String } },
+    components: { aic, nic },
 
     data: () => ({
+        product: null,
+    }),
 
-    })
+    mounted() {
+        get_product_by_id(this.id).then(p => this.product = p);
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+.product-page {
+    .product-images-container, .price-container { margin-bottom: 1rem }
 
+    .price-label { font-size: 140% }
+    .price { font-size: 200% }
+
+    .stars {
+        color: grey;
+    }
+}
+
+.accessible-images { display: none }
+.accessible {
+    .accessible-images { display: block }
+    .nonaccessible-images { display: none }
+}
 </style>
---
+++ src/views/guest/explore/product/AccessibleImagesCarousel.vue
@@ -0,0 +1,26 @@
+import get_accessible_images from '@/services/api/products/get_accessible_images';
+<template>
+    <b-carousel
+    id="carousel-1"
+    :interval="5000"
+    controls
+    indicators
+    style="text-shadow: 1px 1px 2px #333;">
+        <!-- Text slides with image -->
+        <b-carousel-slide :img-src="config.api_root + '/products/' + productId + '/images/' + image._id + '/file'" v-for="image in images" v-bind:key="image._id"></b-carousel-slide>
+    </b-carousel>
+</template>
+
+<script>
+import get_accessible_images from '@/services/api/products/get_accessible_images'
+import config from '@/config.json'
+
+export default {
+    props: { productId: { required: true, type: String } },
+    data: () => ({ images: [], config }),
+
+    mounted() {
+        get_accessible_images(this.productId).then(i => this.images = i);
+    }
+}
+</script>
