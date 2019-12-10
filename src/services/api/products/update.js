943884cc3a2425592bec30685462a4ab581f0d0c
Marius Drąsutis <marius.dras@gmail.com>
2019-12-10 20:46:18 +0200

    update product API call

---
+++ src/services/api/products/update.js
@@ -0,0 +1,12 @@
+import client from '../client'
+
+/**
+ * @returns {Promise} Resolves with whatever the API sends back.
+ */
+export default (id, properties) => {
+    return client.post('/products/'+id, properties)
+        .then(response => (true))
+        .catch(e => {
+            throw new Error(e.response ? e.response.data.err : e.message);
+        });
+}
