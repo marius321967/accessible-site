be8dc5e62bba93a5affdadcf6623e5d3e8751cc1
Marius Drąsutis <marius.dras@gmail.com>
2019-12-10 20:45:06 +0200

    get accessible/nonaccessible images API calls

---
+++ src/services/api/products/get_accessible_images.js
@@ -0,0 +1,12 @@
+import client from '../client'
+
+/**
+ * @returns {Promise}
+ */
+export default productId => {
+    return client.get('/products/' + productId + '/accessible-images')
+        .then(response => (response.data))
+        .catch(e => {
+            throw new Error(e.response ? e.response.data.err : e.message);
+        });
+}
