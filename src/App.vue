8a40fadc9f157529e5d00a0be51c643b3443b43d
Marius Drąsutis <marius.dras@gmail.com>
2019-12-10 20:50:08 +0200

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-    <div>
+    <div :class="appClasses">
         <router-view />
         
-        <b-popover :target="popoverElement" v-if="popoverElement" :show="true" triggers="manual" placement="bottomleft">
-            <template v-slot:title>Popover Title</template>
-            <button>Speak</button>
+        <b-popover :target="popoverElement" v-if="accessibleMode && popoverElement" :show="true" triggers="manual" placement="bottom">
+            <template v-slot:title>Help</template>
+            <button class="btn btn-secondary btn-sm" @click="speakSelection">Speak</button>
         </b-popover>
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import speak from '@/services/accessibility/speak'
 
 export default {
     beforeCreate() {
@@ -20,21 +21,35 @@ export default {
         const store = this.$store;
 
         document.addEventListener('selectionchange', e => {
-            const isDeselected = document.getSelection().toString() == '';
-            const element = document.getSelection().focusNode.parentElement;
-            // console.log({ top: element.offsetTop, left: element.offsetLeft });
+            const currentElement = this.popoverElement;
+            const selection = document.getSelection();
+            const isDeselected = selection.toString() == '';
 
-            if (isDeselected)   store.commit('POPOVER_SET_ELEMENT', null);
-            else                store.commit('POPOVER_SET_ELEMENT', element);
+            if (isDeselected) store.commit('POPOVER_SET_ELEMENT', null);
+            else if (currentElement != selection.focusNode.parentElement) store.commit('POPOVER_SET_ELEMENT', selection.focusNode.parentElement);
         });
     },
 
-    computed: { ...mapGetters(['popoverElement']) }
+    methods: {
+        speakSelection() {
+            speak( document.getSelection().toString() );
+        }
+    },
+
+    computed: { 
+        ...mapGetters(['popoverElement', 'accessibleMode']),
+        appClasses() {
+            const c = [];
+            if (this.accessibleMode) c.push('accessible');
+            return c;
+        }
+    }
 }
 </script>
 
 <style lang="scss">
 @import 'bootstrap';
+@import '@/assets/accessible';
 
 body {
     font-weight: 500;
@@ -45,18 +60,7 @@ body {
     margin-top: 1rem;
 }
 
-label.label {
-    text-transform: uppercase;
-}
-
-.form-group {
-    margin-bottom: 2rem;
-}
-
-.primary-links .nav-item .nav-link {
-    text-transform: uppercase;
-    letter-spacing: 0.2rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
+.router-link-active {
+    @extend .active;
 }
 </style>
