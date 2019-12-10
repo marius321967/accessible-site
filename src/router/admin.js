6cf8fd1d0b8edb16640578f51655109ad185a0e9
Marius Drąsutis <marius.dras@gmail.com>
2019-12-10 20:49:28 +0200

    Updated admin routes list

--- src/router/admin.js
+++ src/router/admin.js
@@ -4,6 +4,9 @@ import Users from '@/views/admin/Users'
 import Products from '@/views/admin/Products'
 import ProductsNew from '@/views/admin/products/New'
 import ProductsView from '@/views/admin/products/View'
+import Categories from '@/views/admin/Categories'
+import CategoriesNew from '@/views/admin/categories/New'
+import CategoriesView from '@/views/admin/categories/View'
 
 export default {
     path: '/admin',
@@ -14,5 +17,8 @@ export default {
         { path: 'products', component: Products },
         { path: 'products/new', component: ProductsNew },
         { path: 'products/:id', component: ProductsView, props: true },
+        { path: 'categories', component: Categories },
+        { path: 'categories/new', component: CategoriesNew },
+        { path: 'categories/:id', component: CategoriesView, props: true },
     ]
 }
