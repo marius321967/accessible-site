b5c93d0b089631c7a66e3b7226813fea38f5fcf1
Marius Drąsutis <marius.dras@gmail.com>
2019-12-10 20:49:18 +0200

    Updated admin navigation for categories

--- src/views/nav/admin/Navigation.vue
+++ src/views/nav/admin/Navigation.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div class="nav flex-column nav-pills">
-            <router-link to="/admin" class="nav-link active">Home</router-link>
+            <router-link to="/" class="nav-link">Site</router-link>
+            <router-link to="/admin" class="nav-link">Home</router-link>
+            <router-link to="/admin/categories" class="nav-link">Categories</router-link>
             <router-link to="/admin/products" class="nav-link">Products</router-link>
             <router-link to="/admin/users" class="nav-link">Users</router-link>
         </div>
