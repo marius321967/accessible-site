5cad4812c1ca4b64bb120972d3b0636c5a35ae5b
Marius Drąsutis <marius.dras@gmail.com>
2019-12-17 20:19:53 +0200

    -

--- src/views/nav/admin/Navigation.vue
+++ src/views/nav/admin/Navigation.vue
@@ -6,6 +6,7 @@
             <router-link to="/admin/categories" class="nav-link">Categories</router-link>
             <router-link to="/admin/products" class="nav-link">Products</router-link>
             <router-link to="/admin/users" class="nav-link">Users</router-link>
+            <router-link to="/admin/orders" class="nav-link">Orders</router-link>
         </div>
     </div>
 </template>
